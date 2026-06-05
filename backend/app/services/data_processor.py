56235252aa9c0e61456d1e17f70377ab20e8cff4
kashvi456 <jkashvi22@gmail.com>
2026-06-05 16:16:40 +0530

    Data Processor

---
+++ backend/app/services/data_processor.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+class DataProcessor:
+    @staticmethod
+    def preprocess_transactions(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Cleans and transforms raw Paysim transaction data into
+        numerical vectors ready for the Isolation Forest model.
+        """
+        processed_df = df.copy()
+
+        features = ['amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest','newbalanceDest']
+
+        processed_df = processed_df.dropna(subset=features)
+
+        if 'type' in processed_df.columns:
+            type_dummies = pd.get_dummies(processed_df['type'],  prefix='type', drop_first=True)
+            processed_df = pd.concat([processed_df[features], type_dummies], axis=1)
+        else:
+            processed_df = processed_df[features]
+        
+        processed_df = processed_df.astype(float)
+
+        return processed_df
+
